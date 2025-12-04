b08ee7c1bbb530b90bcb4586d740cec1b73c1e23
Andre <Andre.v2005@aluno.ifsc.edu.br>
2025-12-04 11:28:03 -0300

    Finalizado

--- app/src/main/java/com/example/contadordeclicks/MainActivity.java
+++ app/src/main/java/com/example/contadordeclicks/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.contadordeclicks;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int cliques = 0;
+    TextView contagem;
+    Button Botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        System.out.println("");
+        contagem = findViewById(R.id.textView);
+        Botao = findViewById(R.id.button);
+        Botao.setOnClickListener(botao -> {
+            cliques++;
+            contagem.setText(String.valueOf(cliques));
+        });
+
+
     }
 }
